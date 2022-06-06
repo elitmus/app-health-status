2267c5af068a9cf353501e809277a809a2cf8a37
Nikhil Bhatt <nikhilbhatt931@gmail.com>
2022-06-06 16:53:09 +0530

    [UPDATE] app-health-status path host whitelisted

--- lib/app_health_status/railtie.rb
+++ lib/app_health_status/railtie.rb
@@ -5,6 +5,12 @@ module AppHealthStatus
   class Railtie < ::Rails::Railtie
     initializer 'app_health_status.configure_rails_initialization' do
       Rails.application.middleware.use Rack::AppHealthStatusMiddleware
+
+      if Rails.application.config.respond_to?(:host_authorization) && Rails.application.config.host_authorization[:exclude].nil?
+        Rails.application.config.host_authorization = {
+          exclude: ->(request) { request.path.include?(AppHealthStatus.configuration.path) }
+        }
+      end
     end
   end
 end
--- spec/app_health_status_routing_spec.rb
+++ spec/app_health_status_routing_spec.rb
@@ -1,25 +1,35 @@
 RSpec.describe Rack::AppHealthStatusMiddleware do
-  let(:app) { ->(env) { [404, env, "app"] } }
+  let(:app) { ->(env) { [env['HOST'] && env['HOST'] != env['SERVER_NAME'] ? 403 : 404, env, 'app'] } }
   let(:middleware) do
     Rack::AppHealthStatusMiddleware.new(app)
   end
 
   it 'should return 200 when path matches' do
-    code, env = middleware.call env_for('https://www.pacexam.com/app-health-status')
+    code, env = middleware.call env_for('https://www.elitmus.com/app-health-status')
     expect(code).to eq(200)
     user_defined_path = 'some-random-path'
     AppHealthStatus.configuration.path = user_defined_path
-    code, env = middleware.call env_for("https://www.pacexam.com/#{user_defined_path}")
+    code, env = middleware.call env_for("https://www.elitmus.com/#{user_defined_path}")
     expect(code).to eq(200)
     AppHealthStatus.reset
   end
 
   it 'should not interfere when path does matches' do
-    code, env = middleware.call env_for('https://www.pacexam.com/some-random-path')
+    code, env = middleware.call env_for('https://www.elitmus.com/some-random-path')
     expect(code).to eq(404)
   end
 
-  def env_for url
-    Rack::MockRequest.env_for(url)
+  it 'should give 403 when host is differnt and path is not whitelisted' do
+    code, env = middleware.call env_for('https://www.elitmus.com/some-random-path', {
+      'HTTP_X_FORWARDED_HOST' => '//randomhost.com',
+      'HOST' => 'www.randomhost.com',
+      'action_dispatch.show_detailed_exceptions' => true
+    })
+    
+    expect(code).to eq(403)
+  end
+
+  def env_for url, opts = {}
+    Rack::MockRequest.env_for(url, opts)
   end
 end
